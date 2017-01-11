ed4b8ac62814eb7a2ff195b8da0aa290c8862234
oltima <sjh8543@gmail.com>
2017-01-11 15:32:24 +0900

    change running format from simple python file to using tf.app format

--- distributed_version/SimpleDistServer.py
+++ distributed_version/SimpleDistServer.py
@@ -1,13 +1,28 @@
-# Get task number from command line
-import sys
-task_number = int(sys.argv[1])
-
 import tensorflow as tf
 
-cluster = tf.train.ClusterSpec({"local": ["localhost:2222", "localhost:2223"]})
-server = tf.train.Server(cluster, job_name="local", task_index=task_number)
+#Flags for defining the tf.train.ClusterSpec
+tf.app.flags.DEFINE_string("ps_hosts","",
+                           "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("worker_hosts","",
+                           "Comma-separated list of hostname:port paris")
 
-print("Starting server #{}".format(task_number))
+#Flags for defining the tf.train.Server
+tf.app.flags.DEFINE_string("job_name","","One of 'ps' , 'worker'")
+tf.app.flags.DEFINE_integer("task_index",0,"Index of task within the job") 
 
-server.start()
-server.join()
+FLAGS = tf.app.flags.FLAGS
+
+#define main function for ditributed server
+def main(_):
+    ps_hosts = FLAGS.ps_hosts.split(",")
+    worker_hosts = FLAGS.worker_hosts.split(",")
+    cluster = tf.train.ClusterSpec( {"ps":ps_hosts,"worker":worker_hosts} )
+    server = tf.train.Server( cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index )
+
+    print("Starting server #{}".format(FLAGS.task_index))
+
+    server.start()
+    server.join()
+
+if __name__ == "__main__":
+    tf.app.run()
